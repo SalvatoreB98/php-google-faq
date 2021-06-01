82c462932abf639c4ff904eb3f393c89bf909bdf
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-01 16:46:02 +0200

    Esrcizio prima parte OK

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,6 @@
+// Importa la libreria che vogliamo usare
 let mix = require('laravel-mix');
 
-mix.sass("src/style.scss", "dist/")
+// Specifica che vogliamo compilare il file src/app.scss 
+// e che la versione compilata verrà salvata all'interno della cartella dist/
+mix.sass("src/app.scss", "dist/")
